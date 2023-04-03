2334e9fc02a70278a295f8293783853ed66ac2f8
Vadim Bubnov <banzabit@gmail.com>
2023-04-03 14:58:16 -0300

    Auto complete app

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# auto_complete
+# Auto complete
+Store the result of the request and display the information in a grid of cards styled as seen below. This app will load a list of users from an API
+
+* To see how autocomplete process works, you would have to write a name or a country in the search part. User information can be editable clicking on the edit button.
+
+# Scripts
+Install dependencies:
+* yarn install
+
+Run App:
+* yarn start
+
+Run tests:
+Run all test >
+* yarn test
+Run one suit of test >
+* yarn test:suit src/components/Card.test.tsx
+
+Coverage:
+Get coverage for all files >
+* yarn coverage
+Get coverage for one suit of test >
+* yarn coverage:suit
+
+Lint:
+* yarn lint
+
+Clear cache of lint:
+* yarn clear:jest
---
+++ src/components/Users.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { AppContext } from '../context/AppContext'
+import Card from './Card'
+
+import { getAllUsers } from '../api/getUsers'
+import './Users.scss'
+
+const Users = () => {
+    // Users is the section where we show all user information like card
+    const {
+        setUserListContext,
+        setUserListCopy,
+        userList,
+    } = React.useContext(AppContext)
+
+    const handleGetAllUsers = async() => {
+        // Calling the API to get the users
+        const allAsersResponse = await getAllUsers()
+
+        // We store the information if the response status is ok 
+        if(allAsersResponse.status) {
+            const data: any = allAsersResponse.data
+            setUserListContext(data)
+            setUserListCopy([...data])
+        } else {
+            alert(allAsersResponse.mensaje)
+        }
+    }
+
+    // Scroll to top of page. Is good approach when we have big list to show
+    const handleGoTop = () => (
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    )
+
+    // Call get users method. Will be called ones
+    React.useEffect(() => {
+        handleGetAllUsers()
+    }, [])
+
+    return (
+        <div className='cardSection'>
+            <div className='cardItems'>
+                {userList.map((item) => (
+                   <Card key={item.userId} item={item} />
+                ))}
+            </div>
+            <button className="arrowUp" onClick={handleGoTop}>&uarr;</button>
+        </div>
+    )
+}
+
+export default Users
